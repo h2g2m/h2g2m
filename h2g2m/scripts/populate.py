0435b64b51a673f7a6ee08cca9af853e89a01573
Nikolai <mail@nikno.de>
2015-08-28 21:26:32 +0100

    commented out problematic parts of populate.py

--- h2g2m/scripts/populate.py
+++ h2g2m/scripts/populate.py
@@ -123,155 +123,155 @@ def main(argv=sys.argv):
         DBSession.add(T)
         DBSession.flush()
         #Add Annotations to that text
-        A = Annotation(content='the $\infty$ should be an $n$', location='page 3, line 6', language='Deutsch')
-        A.creator = adm_usr
-        A.txts.append(T)
-        A.tags = 'remark'
-        DBSession.add(A)
-        B = Annotation(content='a second annotation', location='page 234, line 43', language='English')
-        B.creator = adm_usr
-        B.txts.append(T)
-        B.tags = 'unclear'
-        DBSession.add(B)
-        DBSession.flush()
-        #Create A TeX Header for our user
-        texh = TexHeader(name='MyDefaultHeader', content=r'\newcommand{\N}{\mathbb N}')
-        texh.creator = adm_usr
-        DBSession.add(texh)
-        DBSession.flush()
-        #Create a TeX Header for the other user
-        texhother = TexHeader(name='MyOtherUser', content=r'\newcommand{\Z}{\mathbb Z}')
-        texhother.creator = other_usr
-        DBSession.add(texhother)
-        DBSession.flush()
-        #Add several posts
-        post1 = Post(content='1) Cool Annotation. Look at my cool LaTeX Header: $\N$!', is_resolution=False,
-                     parent_id=False)
-        post1.creator = adm_usr
-        post1.annotation = A
-        post1.tex_header = texh
-        DBSession.add(post1)
-        DBSession.flush()
-        post2 = Post(content='2) In this post, my LaTeX Header is disabled. $\N$', is_resolution=False)
-        post2.creator = adm_usr
-        post2.parent_id = post1.id
-        DBSession.add(post2)
-        DBSession.flush()
-        post3 = Post(content='3) This post refers to my post 2).', is_resolution=False)
-        post3.creator = adm_usr
-        post3.parent_id = post2.id
-        DBSession.add(post3)
-        DBSession.flush()
-        post4 = Post(content='4) This post refers to my post 1).', is_resolution=False)
-        post4.creator = adm_usr
-        post4.parent_id = post1.id
-        DBSession.add(post4)
-        DBSession.flush()
-        post5 = Post(content='5) This post refers to my post 4).', is_resolution=False)
-        post5.creator = adm_usr
-        post5.parent_id = post4.id
-        DBSession.add(post5)
-        DBSession.flush()
-
-        #Add some users, Txts and annotations for Authorization Testing
-        bob = Usr(nickname='bob', login='bob', passwd='bob', language='English', grp_id=bob.id)
-        DBSession.add(bob)
-        hartshorne = Person(name='Robin Hartshorne')
-        hefner = Person(name='Hugh Hefner')
-        DBSession.add(hartshorne)
-        DBSession.add(hefner)
-        DBSession.flush()
-        AlgGeo = Txt(title='Algebraic Geometry', edition='1', isbn='1337', creator=bob, language='English')
-        AlgGeo.author_list.append(hartshorne)
-        DBSession.add(AlgGeo)
-        Playboy = Txt(title='Playboy', edition='1', isbn='1337', creator=bob, language='English')
-        Playboy.author_list.append(hefner)
-        DBSession.add(Playboy)
-        DBSession.flush()
-        A = Annotation(content='the $\infty$ should be an $n$', title='world,world', location='page 3, line 6',
-                       language='English')
-        A.creator = bob
-        A.txts.append(AlgGeo)
-        A.tags = 'remark'
-        #DBSession.add(A)
-        B = Annotation(content='you cannot edit this', location='page 3, line 6', title='world,usr', language='English')
-        B.creator = bob
-        B.txts.append(AlgGeo)
-        B.tags = 'remark'
-        DBSession.add(B)
-        C = Annotation(content='this book sucks', location='page 3, line 6', language='English', title='usr, usr')
-        C.creator = bob
-        C.txts.append(AlgGeo)
-        C.tags = 'remark'
-        DBSession.add(C)
-        DBSession.flush()
-        AP = Annotation(content='the $\infty$ should be an $n$', title='world,world', location='page 3, line 6',
-                        language='English')
-        AP.creator = bob
-        AP.txts.append(Playboy)
-        AP.tags = 'remark'
-        DBSession.add(AP)
-        BP = Annotation(content='you cannot edit this', location='page 3, line 6', title='world,usr',
-                        language='English')
-        BP.creator = bob
-        BP.txts.append(Playboy)
-        BP.tags = 'remark'
-        DBSession.add(BP)
-        CP = Annotation(content='this book sucks', location='page 3, line 6', language='English', title='usr, usr')
-        CP.creator = bob
-        CP.txts.append(Playboy)
-        CP.tags = 'remark'
-        DBSession.add(CP)
-        DBSession.flush()
-
-        alice = Usr(login='alice', nickname='alice', passwd='alice', language='English', grp_id=alice.id)
-        DBSession.add(alice)
-        hoermander = Person(name='Lars hoermander')
-        meyer = Person(name='Stephanie Meyer')
-        DBSession.add(hoermander)
-        DBSession.add(meyer)
-        DBSession.flush()
-        PDO = Txt(title='Linear Partial Differential Operators', edition='1', isbn='1337', creator=alice,
-                  language='English')
-        PDO.author_list.append(hoermander)
-        DBSession.add(PDO)
-        twilight = Txt(title='Twilight', edition='1', isbn='1337', creator=alice, language='English')
-        twilight.author_list.append(meyer)
-        DBSession.add(twilight)
-        DBSession.flush()
-        aA = Annotation(content='the $\infty$ should be an $n$', title='world,world', location='page 3, line 6',
-                        language='English')
-        aA.creator = alice
-        aA.txts.append(PDO)
-        aA.tags = 'remark'
-        DBSession.add(aA)
-        aB = Annotation(content='you cannot edit this', location='page 3, line 6', title='world,usr',
-                        language='English')
-        aB.creator = alice
-        aB.txts.append(PDO)
-        aB.tags = 'remark'
-        DBSession.add(aB)
-        aC = Annotation(content='this book sucks', location='page 3, line 6', language='English', title='usr, usr')
-        aC.creator = alice
-        aC.txts.append(PDO)
-        aC.tags = 'remark'
-        DBSession.add(aC)
-        DBSession.flush()
-        aAP = Annotation(content='the $\infty$ should be an $n$', title='world,world', location='page 3, line 6',
-                         language='English')
-        aAP.creator = alice
-        aAP.txts.append(twilight)
-        aAP.tags = 'remark'
-        DBSession.add(aAP)
-        aBP = Annotation(content='you cannot edit this', location='page 3, line 6', title='world,usr',
-                         language='English')
-        aBP.creator = alice
-        aBP.txts.append(twilight)
-        aBP.tags = 'remark'
-        DBSession.add(aBP)
-        aCP = Annotation(content='this book sucks', location='page 3, line 6', language='English', title='usr, usr')
-        aCP.creator = alice
-        aCP.txts.append(twilight)
-        aCP.tags = 'remark'
-        DBSession.add(aCP)
-        DBSession.flush()
+        # A = Annotation(content='the $\infty$ should be an $n$', location='page 3, line 6', language='Deutsch')
+        # A.creator = adm_usr
+        # A.txts.append(T)
+        # A.tags = 'remark'
+        # DBSession.add(A)
+        # B = Annotation(content='a second annotation', location='page 234, line 43', language='English')
+        # B.creator = adm_usr
+        # B.txts.append(T)
+        # B.tags = 'unclear'
+        # DBSession.add(B)
+        # #DBSession.flush()
+        # Create A TeX Header for our user
+        # texh = TexHeader(name='MyDefaultHeader', content=r'\newcommand{\N}{\mathbb N}')
+        # texh.creator = adm_usr
+        # DBSession.add(texh)
+        # DBSession.flush()
+        # #Create a TeX Header for the other user
+        # texhother = TexHeader(name='MyOtherUser', content=r'\newcommand{\Z}{\mathbb Z}')
+        # texhother.creator = other_usr
+        # DBSession.add(texhother)
+        # DBSession.flush()
+        # #Add several posts
+        # post1 = Post(content='1) Cool Annotation. Look at my cool LaTeX Header: $\N$!', is_resolution=False,
+        #              parent_id=False)
+        # post1.creator = adm_usr
+        # post1.annotation = A
+        # post1.tex_header = texh
+        # DBSession.add(post1)
+        # DBSession.flush()
+        # post2 = Post(content='2) In this post, my LaTeX Header is disabled. $\N$', is_resolution=False)
+        # post2.creator = adm_usr
+        # post2.parent_id = post1.id
+        # DBSession.add(post2)
+        # DBSession.flush()
+        # post3 = Post(content='3) This post refers to my post 2).', is_resolution=False)
+        # post3.creator = adm_usr
+        # post3.parent_id = post2.id
+        # DBSession.add(post3)
+        # DBSession.flush()
+        # post4 = Post(content='4) This post refers to my post 1).', is_resolution=False)
+        # post4.creator = adm_usr
+        # post4.parent_id = post1.id
+        # DBSession.add(post4)
+        # DBSession.flush()
+        # post5 = Post(content='5) This post refers to my post 4).', is_resolution=False)
+        # post5.creator = adm_usr
+        # post5.parent_id = post4.id
+        # DBSession.add(post5)
+        # DBSession.flush()
+        #
+        # #Add some users, Txts and annotations for Authorization Testing
+        # bob = Usr(nickname='bob', login='bob', passwd='bob', language='English', grp_id=bob.id)
+        # DBSession.add(bob)
+        # hartshorne = Person(name='Robin Hartshorne')
+        # hefner = Person(name='Hugh Hefner')
+        # DBSession.add(hartshorne)
+        # DBSession.add(hefner)
+        # DBSession.flush()
+        # AlgGeo = Txt(title='Algebraic Geometry', edition='1', isbn='1337', creator=bob, language='English')
+        # AlgGeo.author_list.append(hartshorne)
+        # DBSession.add(AlgGeo)
+        # Playboy = Txt(title='Playboy', edition='1', isbn='1337', creator=bob, language='English')
+        # Playboy.author_list.append(hefner)
+        # DBSession.add(Playboy)
+        # DBSession.flush()
+        # A = Annotation(content='the $\infty$ should be an $n$', title='world,world', location='page 3, line 6',
+        #                language='English')
+        # A.creator = bob
+        # A.txts.append(AlgGeo)
+        # A.tags = 'remark'
+        # #DBSession.add(A)
+        # B = Annotation(content='you cannot edit this', location='page 3, line 6', title='world,usr', language='English')
+        # B.creator = bob
+        # B.txts.append(AlgGeo)
+        # B.tags = 'remark'
+        # DBSession.add(B)
+        # C = Annotation(content='this book sucks', location='page 3, line 6', language='English', title='usr, usr')
+        # C.creator = bob
+        # C.txts.append(AlgGeo)
+        # C.tags = 'remark'
+        # DBSession.add(C)
+        # DBSession.flush()
+        # AP = Annotation(content='the $\infty$ should be an $n$', title='world,world', location='page 3, line 6',
+        #                 language='English')
+        # AP.creator = bob
+        # AP.txts.append(Playboy)
+        # AP.tags = 'remark'
+        # DBSession.add(AP)
+        # BP = Annotation(content='you cannot edit this', location='page 3, line 6', title='world,usr',
+        #                 language='English')
+        # BP.creator = bob
+        # BP.txts.append(Playboy)
+        # BP.tags = 'remark'
+        # DBSession.add(BP)
+        # CP = Annotation(content='this book sucks', location='page 3, line 6', language='English', title='usr, usr')
+        # CP.creator = bob
+        # CP.txts.append(Playboy)
+        # CP.tags = 'remark'
+        # DBSession.add(CP)
+        # DBSession.flush()
+        #
+        # alice = Usr(login='alice', nickname='alice', passwd='alice', language='English', grp_id=alice.id)
+        # DBSession.add(alice)
+        # hoermander = Person(name='Lars hoermander')
+        # meyer = Person(name='Stephanie Meyer')
+        # DBSession.add(hoermander)
+        # DBSession.add(meyer)
+        # DBSession.flush()
+        # PDO = Txt(title='Linear Partial Differential Operators', edition='1', isbn='1337', creator=alice,
+        #           language='English')
+        # PDO.author_list.append(hoermander)
+        # DBSession.add(PDO)
+        # twilight = Txt(title='Twilight', edition='1', isbn='1337', creator=alice, language='English')
+        # twilight.author_list.append(meyer)
+        # DBSession.add(twilight)
+        # DBSession.flush()
+        # aA = Annotation(content='the $\infty$ should be an $n$', title='world,world', location='page 3, line 6',
+        #                 language='English')
+        # aA.creator = alice
+        # aA.txts.append(PDO)
+        # aA.tags = 'remark'
+        # DBSession.add(aA)
+        # aB = Annotation(content='you cannot edit this', location='page 3, line 6', title='world,usr',
+        #                 language='English')
+        # aB.creator = alice
+        # aB.txts.append(PDO)
+        # aB.tags = 'remark'
+        # DBSession.add(aB)
+        # aC = Annotation(content='this book sucks', location='page 3, line 6', language='English', title='usr, usr')
+        # aC.creator = alice
+        # aC.txts.append(PDO)
+        # aC.tags = 'remark'
+        # DBSession.add(aC)
+        # DBSession.flush()
+        # aAP = Annotation(content='the $\infty$ should be an $n$', title='world,world', location='page 3, line 6',
+        #                  language='English')
+        # aAP.creator = alice
+        # aAP.txts.append(twilight)
+        # aAP.tags = 'remark'
+        # DBSession.add(aAP)
+        # aBP = Annotation(content='you cannot edit this', location='page 3, line 6', title='world,usr',
+        #                  language='English')
+        # aBP.creator = alice
+        # aBP.txts.append(twilight)
+        # aBP.tags = 'remark'
+        # DBSession.add(aBP)
+        # aCP = Annotation(content='this book sucks', location='page 3, line 6', language='English', title='usr, usr')
+        # aCP.creator = alice
+        # aCP.txts.append(twilight)
+        # aCP.tags = 'remark'
+        # DBSession.add(aCP)
+        # DBSession.flush()
